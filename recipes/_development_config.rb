ec0e39f90a4fca61b0bf51d6a57f05b138911209
Patrick Leary <patrick.r.leary@gmail.com>
2014-10-03 19:45:37 -0400

    Updating some paths

--- recipes/_development_config.rb
+++ recipes/_development_config.rb
@@ -17,57 +17,57 @@
 # limitations under the License.
 #
 
-execute "sudo -u vagrant cp /home/vagrant/rails/inaturalist/config/config.yml.example /home/vagrant/rails/inaturalist/config/config.yml" do
-  only_if { File.exists?("/home/vagrant/rails/inaturalist/config/config.yml.example") }
-  not_if { File.exists?("/home/vagrant/rails/inaturalist/config/config.yml") }
+execute "sudo -u vagrant cp /vagrant/shared/rails/inaturalist/config/config.yml.example /vagrant/shared/rails/inaturalist/config/config.yml" do
+  only_if { File.exists?("/vagrant/shared/rails/inaturalist/config/config.yml.example") }
+  not_if { File.exists?("/vagrant/shared/rails/inaturalist/config/config.yml") }
   notifies :run, 'script[edit_config_yml]', :delayed
 end
 
-execute "sudo -u vagrant cp /home/vagrant/rails/inaturalist/config/database.yml.example /home/vagrant/rails/inaturalist/config/database.yml" do
-  only_if { File.exists?("/home/vagrant/rails/inaturalist/config/database.yml.example") }
-  not_if { File.exists?("/home/vagrant/rails/inaturalist/config/database.yml") }
+execute "sudo -u vagrant cp /vagrant/shared/rails/inaturalist/config/database.yml.example /vagrant/shared/rails/inaturalist/config/database.yml" do
+  only_if { File.exists?("/vagrant/shared/rails/inaturalist/config/database.yml.example") }
+  not_if { File.exists?("/vagrant/shared/rails/inaturalist/config/database.yml") }
   notifies :run, 'script[edit_database_yml]', :delayed
 end
 
-execute "sudo -u vagrant cp /home/vagrant/rails/inaturalist/config/gmaps_api_key.yml.example /home/vagrant/rails/inaturalist/config/gmaps_api_key.yml" do
-  only_if { File.exists?("/home/vagrant/rails/inaturalist/config/gmaps_api_key.yml.example") }
-  not_if { File.exists?("/home/vagrant/rails/inaturalist/config/gmaps_api_key.yml") }
+execute "sudo -u vagrant cp /vagrant/shared/rails/inaturalist/config/gmaps_api_key.yml.example /vagrant/shared/rails/inaturalist/config/gmaps_api_key.yml" do
+  only_if { File.exists?("/vagrant/shared/rails/inaturalist/config/gmaps_api_key.yml.example") }
+  not_if { File.exists?("/vagrant/shared/rails/inaturalist/config/gmaps_api_key.yml") }
 end
 
-execute "sudo -u vagrant cp /home/vagrant/rails/inaturalist/config/smtp.yml.example /home/vagrant/rails/inaturalist/config/smtp.yml" do
-  only_if { File.exists?("/home/vagrant/rails/inaturalist/config/smtp.yml.example") }
-  not_if { File.exists?("/home/vagrant/rails/inaturalist/config/smtp.yml") }
+execute "sudo -u vagrant cp /vagrant/shared/rails/inaturalist/config/smtp.yml.example /vagrant/shared/rails/inaturalist/config/smtp.yml" do
+  only_if { File.exists?("/vagrant/shared/rails/inaturalist/config/smtp.yml.example") }
+  not_if { File.exists?("/vagrant/shared/rails/inaturalist/config/smtp.yml") }
 end
 
-execute "sudo -u vagrant cp /home/vagrant/rails/inaturalist/config/sphinx.yml.example /home/vagrant/rails/inaturalist/config/sphinx.yml" do
-  only_if { File.exists?("/home/vagrant/rails/inaturalist/config/sphinx.yml.example") }
-  not_if { File.exists?("/home/vagrant/rails/inaturalist/config/sphinx.yml") }
+execute "sudo -u vagrant cp /vagrant/shared/rails/inaturalist/config/sphinx.yml.example /vagrant/shared/rails/inaturalist/config/sphinx.yml" do
+  only_if { File.exists?("/vagrant/shared/rails/inaturalist/config/sphinx.yml.example") }
+  not_if { File.exists?("/vagrant/shared/rails/inaturalist/config/sphinx.yml") }
 end
 
 script "edit_config_yml" do
   interpreter "bash"
   user "vagrant"
-  cwd "/home/vagrant/rails/inaturalist"
+  cwd "/vagrant/shared/rails/inaturalist"
   code <<-EOH
-  sed -i 's/http:\\\/\\\/www\\\.yoursite\\\.com/http:\\\/\\\/localhost:3000/g' /home/vagrant/rails/inaturalist/config/config.yml
-  sed -i 's/yourbucketname/staticdev\\\.inaturalist\\\.org/g' /home/vagrant/rails/inaturalist/config/config.yml
+  sed -i 's/http:\\\/\\\/www\\\.yoursite\\\.com/http:\\\/\\\/localhost:3000/g' /vagrant/shared/rails/inaturalist/config/config.yml
+  sed -i 's/yourbucketname/staticdev\\\.inaturalist\\\.org/g' /vagrant/shared/rails/inaturalist/config/config.yml
   EOH
-  only_if { File.exists?("/home/vagrant/rails/inaturalist/config/config.yml") }
-  not_if "grep staticdev /home/vagrant/rails/inaturalist/config/config.yml"
+  only_if { File.exists?("/vagrant/shared/rails/inaturalist/config/config.yml") }
+  not_if "grep staticdev /vagrant/shared/rails/inaturalist/config/config.yml"
   action :nothing
 end
 
 script "edit_database_yml" do
   interpreter "bash"
   user "vagrant"
-  cwd "/home/vagrant/rails/inaturalist"
+  cwd "/vagrant/shared/rails/inaturalist"
   code <<-EOH
-  sed -i 's/host: localhost/host: 192.168.50.7/' /home/vagrant/rails/inaturalist/config/database.yml
-  sed -i 's/username: you/username: postgres/' /home/vagrant/rails/inaturalist/config/database.yml
-  sed -i 's/template_postgis/template_postgis\\\n  password: #{node[:postgresql][:password][:postgres]}/' /home/vagrant/rails/inaturalist/config/database.yml
+  sed -i 's/host: localhost/host: 192.168.50.7/' /vagrant/shared/rails/inaturalist/config/database.yml
+  sed -i 's/username: you/username: postgres/' /vagrant/shared/rails/inaturalist/config/database.yml
+  sed -i 's/template_postgis/template_postgis\\\n  password: #{node[:postgresql][:password][:postgres]}/' /vagrant/shared/rails/inaturalist/config/database.yml
   EOH
-  only_if { File.exists?("/home/vagrant/rails/inaturalist/config/database.yml") }
-  not_if "grep #{node[:postgresql][:password][:postgres]} /home/vagrant/rails/inaturalist/config/database.yml"
+  only_if { File.exists?("/vagrant/shared/rails/inaturalist/config/database.yml") }
+  not_if "grep #{node[:postgresql][:password][:postgres]} /vagrant/shared/rails/inaturalist/config/database.yml"
   action :nothing
   notifies :run, 'rvm_shell[create_databases]', :immediate
 end
@@ -76,7 +76,7 @@ end
 rvm_shell "create_databases" do
   ruby_string "ruby-1.9.3@inaturalist"
   user "vagrant"
-  cwd "/home/vagrant/rails/inaturalist"
+  cwd "/vagrant/shared/rails/inaturalist"
   code <<-EOH
   rake db:setup
   rake db:setup RAILS_ENV=test
